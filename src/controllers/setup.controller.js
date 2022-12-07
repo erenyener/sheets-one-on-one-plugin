846e5d86171ba94e1bf7683a09a74efc5ccedc19
erenyener <mehmeterenyener2@gmail.com>
2022-12-07 11:08:29 +0300

    one on one sheet header styles and some refactorings

--- src/controllers/setup.controller.js
+++ src/controllers/setup.controller.js
@@ -8,12 +8,26 @@ function processForm(data) {
     const oneOnOneSheet = spreadSheet.insertSheet()
     oneOnOneSheet.setName(SHEET_NAMES.OneToOnes);
 
+    
+    setOneonOneSheetHeaders(oneOnOneSheet, data.headers)
+    addPeopleToOneOnOneSheet(oneOnOneSheet, data.people)
+    setOneOneOneSheetStyles(oneOnOneSheet)
+    
+    
+    return true;
+}
+
+function setOneonOneSheetHeaders(oneOnOneSheet, customHeaders) {
+    customHeaders.shift();
     const defaultHeaders = ["Last 1-1 Date", "SpreadSheet", "1-1 Status", "Days Left for Next 1-1"]
-    data.headers.shift();
-    const allHeaders = ["Name", ...data.headers.map(w => w.charAt(0).toUpperCase() + w.slice(1)), ...defaultHeaders.map(w => w.charAt(0).toUpperCase() + w.slice(1))]
+    const allHeaders = ["Name", ...customHeaders.map(w => w.charAt(0).toUpperCase() + w.slice(1)), ...defaultHeaders.map(w => w.charAt(0).toUpperCase() + w.slice(1))]
+
     oneOnOneSheet.appendRow(allHeaders);
 
-    data.people.forEach(person => {
+}
+
+function addPeopleToOneOnOneSheet(oneOnOneSheet, people) {
+    people.forEach(person => {
         if(person.length > 0) {
             let row = [person[0]];
             if(person.length > 1) {
@@ -25,17 +39,18 @@ function processForm(data) {
             oneOnOneSheet.appendRow(row);
         }
     });
+}
 
+function setOneOneOneSheetStyles(oneOnOneSheet) {
+    
+    oneOnOneSheet.setHiddenGridlines(true);
     oneOnOneSheet.autoResizeColumns(1, oneOnOneSheet.getLastColumn());
+
     const headersRange = oneOnOneSheet.getRange(1, 1, 1, oneOnOneSheet.getLastColumn());
+    const headerDataRange = headersRange.getDataRange()
+    headerDataRange.setFontFamilyAndWeight('Calibri', 600).setFontSize('11')
     headersRange.setBackgroundRGB(252, 229, 205);
     headersRange.setBorder(true, true, true, true, true, true, "black", SpreadsheetApp.BorderStyle.SOLID);
-    
-    
-
-    Logger.log(data)
-
-    return true;
 }
 
 
