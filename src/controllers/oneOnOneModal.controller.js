cf44dbebb8739b5b9c5dc694f596014f88872827
erenyener <mehmeterenyener2@gmail.com>
2022-12-11 23:47:23 +0300

    days left added after one on one form submit

--- src/controllers/oneOnOneModal.controller.js
+++ src/controllers/oneOnOneModal.controller.js
@@ -4,7 +4,8 @@ function processFormOneOnOne(formObject) {
     const cellHelper = new CellHelper();
     const sheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName('1-1s');
     const lastColumn = sheet.getLastColumn();
-
+    const userProperties = PropertiesService.getUserProperties();
+    
     if(!formObject) {
       return false;
     }
@@ -15,6 +16,7 @@ function processFormOneOnOne(formObject) {
     if(result.isSuccess) {
         cellHelper.setCellValue(result.personRowNumber, lastColumn-1, result.oneOnOneStatus, SHEET_NAMES.OneToOnes);
         cellHelper.setCellValue(result.personRowNumber, lastColumn-3, result.savedDate, SHEET_NAMES.OneToOnes);
+        cellHelper.setCellValue(result.personRowNumber, lastColumn, userProperties.getProperty('CYCLE_TIME'), SHEET_NAMES.OneToOnes);
     }
 
     return true;
