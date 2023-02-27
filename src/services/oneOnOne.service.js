816b04e13374de375302a4b719a4b7b15309beef
erenyener <mehmeterenyener2@gmail.com>
2023-02-27 10:18:51 +0300

    remove default editor

--- src/services/oneOnOne.service.js
+++ src/services/oneOnOne.service.js
@@ -2,7 +2,7 @@ function OneOnOneService() { }
 
 OneOnOneService.prototype.createInitialOneToOneSpreadSheet = function(personName) {
     const spreadSheetHelper = new SpreadSheetHelper();
-    const url = spreadSheetHelper.create(personName, "initial", ["mehmeterenyener@gmail.com"]);
+    const url = spreadSheetHelper.create(personName, "initial", []);
     return url;
 }
 
